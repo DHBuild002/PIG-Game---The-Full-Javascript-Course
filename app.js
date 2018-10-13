9b655d6d3efeec7d6c3e9b2e9d8c1a43291552f1
DBuild.002 <dhbuild.002@gmail.com>
2018-10-13 17:17:39 +0100

    Multiple Dice, Ability to Set Game Score, Styling
    I have added multiple items to the game this time around:
    
    - Score Limiter
    - An Additional Dice - For bigger Scores
    - Reset Option to play again!
    - Styling to the SetScore button and display changes so that buttons only show when needed

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var p1 = new player("Player 1", 1);
 var p2 = new player("Player 2", 2);
 var activePlayer = null;
 
-// A Blank Slate
+//on pageload  A Blank Slate
 beforeInit();
 //init();
 
@@ -34,24 +34,32 @@ function player(name, id) {
 	};
   self.roll = function() {
     if (gamePlaying) {
-      var dice = Math.floor(Math.random() * 6) + 1;
-      var diceDOM = document.querySelector('.dice')
-      diceDOM.style.display = 'block';
-      diceDOM.src = 'dice-' + dice + '.png';
-      console.log(dice);
+      var randScore = Math.floor(Math.random() * 6) + 1;
+      var randScore2 = Math.floor(Math.random() * 6) + 1;
 
-      if (dice !== 1) {
-        self.roundScore += dice;
-				self.previousScore.push(dice);
-				console.log(self.previousScore);
+      var primaryDi = document.querySelector('.dice');
+      var secondaryDi = document.querySelector('.dice2');
+
+      primaryDi.style.display = 'block';
+      secondaryDi.style.display = 'block';
+      primaryDi.src = 'dice-' + randScore + '.png';
+      secondaryDi.src = 'dice-' + randScore2 + '.png';
+			console.log(randScore);
+			console.log(randScore2);
+
+      if (randScore !== 1 && randScore2 !== 1) {
+        self.roundScore += (randScore + randScore2);
+				self.previousScore.push(randScore, randScore2);
         document.querySelector('#current-' + self.id).textContent = self.roundScore;
+				console.log(self.previousScore);
 				} else {
+
 				self.clearStreak();
         nextPlayer();
       }
     }
   }
-  self.resetToZero = function() {
+  self.duplicateValues = function() {
     if (self.previousScore.slice(-1)[0] === 6 && self.previousScore.slice(-2)[0] === 6) {
       console.log('Duplicate Detected - Player Scrore resetting to 0');
 			self.clearStreak();
@@ -69,9 +77,12 @@ function player(name, id) {
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer.id + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer.id + '-panel').classList.remove('active');
-        document.querySelector('.btn-roll').style.display = 'none';
-        document.querySelector('.btn-hold').style.display = 'none';
-        gamePlaying = false;
+				document.querySelector('.btn-reset').style.display = 'block';
+
+				document.querySelector('.btn-roll').style.display = 'none';
+				document.querySelector('.btn-hold').style.display = 'none';
+				endGame = true;
+
       } else {
 				self.clearStreak();
         nextPlayer();
@@ -81,12 +92,13 @@ function player(name, id) {
 };
 
 // EVENTS
-document.querySelector('.btn-new').addEventListener('click', init);
+document.querySelector('.btn-reset').addEventListener('click', reset);
+document.querySelector('.btn-new').addEventListener('click', start);
 document.querySelector('.btn-limit').addEventListener('click', set);
 document.querySelector('.btn-roll').addEventListener('click', function(){
 	if(gamePlaying){
 		activePlayer.roll();
-		activePlayer.resetToZero();
+		activePlayer.duplicateValues();
 	}
 });
 document.querySelector('.btn-hold').addEventListener('click', function(){
@@ -96,26 +108,53 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 });
 
 // FUNCTIONS
-function set() {
-  var setScore = document.getElementById('setScore').value;
-  Number(setScore);
-  document.getElementById('limit').innerHTML = setScore;
-  init();
+function reset(){
+	if(endGame = true){
+		location.reload();
+	}
 }
 function beforeInit(){
-  if(gamePlaying === false){
 
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
     document.getElementById('current-1').textContent = '-';
     document.getElementById('current-2').textContent = '-';
+		document.getElementById('score-1').textContent = '-';
+		document.getElementById('score-2').textContent = '-';
+
+		document.getElementById('score-limit').style.display = 'none';
+		document.getElementById('score-input').style.display = 'none';
+		document.getElementById('player-buttons').style.display = 'none';
+
     document.querySelector('.player-1-panel').classList.remove('active');
 
-    if(setScore.value === '0'){
-        document.querySelector('.btn-new').style.display = 'none';
-    }
-  }
+}
+function start(){
+	document.getElementById('score-input').style.display = 'block';
+	document.querySelector('.btn-limit').style.display = 'block';
+
+
+	document.querySelector('.btn-reset').style.display = 'none';
+	document.querySelector('.btn-new').style.display = 'none';
+}
+function set(){
+	var setScoreValue = document.getElementById('setScore').value
+	Number(setScoreValue);
+	if(setScoreValue >= 6){
+		document.getElementById('limit').innerHTML = setScoreValue;
+		document.getElementById('score-limit').style.display = 'block';
+
+	  document.querySelector('.btn-limit').style.display = 'none';
+		document.getElementById('score-input').style.display = 'none';
+		init();
+	} else {
+        alert("Score must be higher than 6!");
+				setScoreValue = "0";
+				start();
+			}
 }
 function init() {
+
   activePlayer = p1;
   gamePlaying = true;
 
@@ -125,12 +164,11 @@ function init() {
   document.getElementById('current-2').textContent = '0';
   document.getElementById('name-1').textContent = 'Player 1';
   document.getElementById('name-2').textContent = 'Player 2';
-  document.querySelector('.btn-new').style.display = 'block';
 
   document.querySelector('.player-1-panel').classList.remove('winner');
   document.querySelector('.player-2-panel').classList.remove('winner');
-	document.querySelector('.btn-roll').style.display = 'block';
-	document.querySelector('.btn-hold').style.display = 'block';
+	document.getElementById('player-buttons').style.display = 'block';
+	document.querySelector('.btn-new').style.display = 'none';
 
   document.querySelector('.player-1-panel').classList.add('active');
 }
@@ -146,6 +184,7 @@ function nextPlayer() {
   document.querySelector('.player-2-panel').classList.toggle('active');
 
   document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.dice2').style.display = 'none';
 }
 function getSum(total, int){
   if(int >= 0){
@@ -165,5 +204,5 @@ DONE!!!!!
 // DONE!!!
 
 // 3. Add another dice to the game, so that there are two dices now.
-// The player looses his current score when one of them is a 1.
+// The player loses his current score when one of them is a 1.
 // (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
